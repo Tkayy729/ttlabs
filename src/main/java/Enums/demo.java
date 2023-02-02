41e2c2a295cb4f37e211c0d817f1f597abb40780
Emmanuel Tweneboah <emmanuel.tweneboah@turntabl.io>
2023-02-02 21:37:04 +0000

    algorithms

---
+++ src/main/java/Enums/demo.java
@@ -0,0 +1,7 @@
+package Enums;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println(Days.valueOf("Monday"));
+    }
+}
--- src/main/java/solvedAlgorithms/Answer.java
+++ src/main/java/solvedAlgorithms/Answer.java
@@ -9,6 +9,52 @@ public class Answer {
 
         createPhoneNumber(nums);
  spinWords("emocleW");
+       String[] mm = new String[]{"A", "B", "C"};
+           whoLikesIt(mm);
+    }
+//
+//            []                                -->  "no one likes this"
+//            ["Peter"]                         -->  "Peter likes this"
+//            ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+//            ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+//            ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+    public static String whoLikesIt(String... names) {
+        switch (names.length) {
+            case 0: return "no one likes this";
+            case 1: return String.format("%s likes this", names[0]);
+            case 2: return String.format("%s and %s like this", names[0], names[1]);
+            case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+        }
+    }
+    public static String whoLikesIt1(String... names) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if(names.length == 0) return "no one likes this";
+        if(names.length == 1) return ""+names[0]+" likes this";
+        if (names.length == 2) {
+            for (int i = 0; i < names.length; i++) {
+                if (i == names.length - 1) stringBuilder.append(names[i]).append(" like this");
+                else stringBuilder.append(names[i]).append(" and ");
+            }
+            return stringBuilder.toString();
+        }
+        if(names.length == 3){
+            for (int i = 0; i < names.length; i++) {
+                if (i == 0) stringBuilder.append(names[i]).append(", ");
+                if (i == 1) stringBuilder.append(names[i]).append(" and ");
+                if(i==2 ) stringBuilder.append(names[i]).append(" like this");
+            }
+            return stringBuilder.toString();
+        }
+        if(names.length > 3){
+        for (int i = 0; i < names.length; i++) {
+            if (i == 0) stringBuilder.append(names[i]).append(", ");
+            if (i == 1) stringBuilder.append(names[i]).append(" and "+(names.length-2)+" like this");
+        }
+        return stringBuilder.toString();
+        }
+        return null;
     }
 
     public static String spinWords(String sentence) {
